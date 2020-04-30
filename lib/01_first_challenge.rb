d4abdbc644bc84a6c0c4cc5541391cd06565bdb1
Ian McKenzie <imckenzie777@gmail.com>
2020-04-30 20:13:58 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -3,31 +3,7 @@ def first_challenge
      :montague => {
        , # dividing family comma
      :capulet => {
-       :patriarch => {
-         :name => 'Lord Capulet',
-         :age => '50'
-       },
-       :matriarch => {
-         :name => 'Lady Capulet',
-         :age => '51'
-       },
-       :heroine => {
-         :name => 'Juliet Capulet',
-         :age => '15',
-         :status => 'alive'
-       },
-       :heroine_friends => [
-         {
-           :name => 'Steven',
-           :age => '30',
-           :attitude => 'confused'
-         },
-         {
-           :name => 'Nurse',
-           :age => '44',
-           :attitude => 'worried'
-         }
-       ]
+
      }
    }
 end
