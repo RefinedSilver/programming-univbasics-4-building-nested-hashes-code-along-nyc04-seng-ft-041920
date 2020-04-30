ae347ab57202374a667620b4f81780b574de8515
Ian McKenzie <imckenzie777@gmail.com>
2020-04-30 20:17:49 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,30 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
-        :name => 'Lord Montague',
-        :age => '53'
-      },
-      :matriarch => {
-        :name => 'Lady Montague',
-        :age => '54'
-      },
-      :hero => {
-        :name => 'Romeo',
-        :age => '15'
-      },
-      :hero_friends => [
-        {
-          :name => 'Benvolio',
-          :age => '17',
-          :attitude => 'worried'
-        },
-        {
-          :name => 'Mercutio',
-          :age => '18',
-          :attitude => 'hot-headed'
-        }
-      ]
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
     }
       },
     :capulet => {
